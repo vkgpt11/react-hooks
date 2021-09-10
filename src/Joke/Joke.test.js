7394d3524b54e8e74a65c2152f813457197356a0
vkgpt11 <vikasgupta.net@live.com>
2021-09-10 11:42:23 +0530

    Added tests to joke and pokemon

--- src/Joke/Joke.test.js
+++ src/Joke/Joke.test.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { render, screen, act } from '@testing-library/react';
+import { render, screen, act, cleanup } from '@testing-library/react';
 import Joke from './Joke';
 
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () =>
-      Promise.resolve({
-        icon_url: 'http://image.png',
-        value: 'Joke text',
-      }),
-  })
-);
-
 describe('Joke', () => {
-  it('loads the joke on mount', async () => {
+  it('Loading message on mount', async () => {
     await act(async () => render(<Joke />));
-    console.log(screen.debug());
-    expect(screen.getByText('Loading ...')).toBeNull();
+    expect(screen.getByText('Loading ...')).toBeInTheDocument();
+    // cleanup();
+  });
+  it('loads the joke on mount', async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () =>
+          Promise.resolve({
+            icon_url: 'http://image.png',
+            value: 'Joke text',
+          }),
+      })
+    );
+    await act(async () => render(<Joke />));
+    expect(screen.getByText('Joke text')).toBeInTheDocument();
+    // cleanup();
   });
 });
